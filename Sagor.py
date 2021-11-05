4f2ecb1492affd258e9dcbade2d796367e103500
Sagor-Ahmed5 <93577598+Sagor-Ahmed5@users.noreply.github.com>
2021-11-05 15:55:51 +0600

    Add files via upload

---
+++ Sagor.py
@@ -0,0 +1,41 @@
+
+# SAGOR AHMED TOOL
+# COMMANDS
+
+````
+$ pkg install python
+
+$ pkg install python2
+
+$ pkg install git
+
+$ pip2 install requests
+
+$ pip2 install mechanize
+
+$pip install requests
+
+$ pip install mechanize
+
+$ git clone https://github.com/Sagor-Ahmed/BD
+
+$ cd Sagor
+
+$ python2 Sagor.py
+
+````
+
+# USERNAME & PASSWORDS
+````
+USERNAME : Sagor
+PASSWORD : Ahmed
+
+
+
+
+# SCREENSHOT
+![](https://d.top4top.io/p_2018b8w5g0.jpg)
+
+### CONTACT WITH  SAGOR AHMED IF YOU NEED ANY HELP
+<b>ðŸ”°ðŸ”°ðŸ”°ðŸ”°</b> </br></b></br> <br>[[![Github](https://img.shields.io/badge/Github-[SAGOR_AHMED]-blue?style=flat-square&logo=GITHUBlogoColor=blue&labelColor=blue)](https://github.com/DevillHunter)] <br> [[![Github](https://img.shields.io/badge/TELEGRAM-[SAGOR_AHMED]-red?style=flat-square&logo=TELEGRAMlogoColor=red&labelColor=cyan)](https://t.me/alamin123khan)]<br> [_[![Facebook](https://img.shields.io/badge/
+
